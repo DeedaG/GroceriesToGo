df30070d2dd90d85cccedde59350c329bc491522
Christia <ChristiaTidwell@gmail.com>
2021-02-15 22:18:17 -0600

    catch up commits

---
+++ GroceriesToGo/ClientApp/src/components/Items.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from 'react';
+import { connect } from 'react-redux';
+
+const Items = () => (
+    <div>
+    <h2>Items</h2>
+            </div>
+);
+
+export default connect()(Items);
